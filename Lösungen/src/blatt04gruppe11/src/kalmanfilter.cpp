23c2a125fd444cfed51e985e872c3a47e0ddb393
mhuendorf <mhuendorf@uos.de>
2021-06-06 15:38:49 +0200

    tried to add functionality of merging two msgs error when binding the callback (kalmanfilter.cpp:38) advise needed

--- Lösungen/src/blatt04gruppe11/src/kalmanfilter.cpp
+++ Lösungen/src/blatt04gruppe11/src/kalmanfilter.cpp
@@ -7,71 +7,70 @@
 #include "sensor_msgs/Imu.h"
 #include "nav_msgs/Odometry.h"
 
+using namespace message_filters;
 
 class Kalmanfilter {
 
-    private:
-    ros::Subscriber sub_imu;
-    ros::Subscriber sub_odom;
-    ros::NodeHandle nh;
 
-    sensor_msgs::Imu imu_msg;
+    private:
+    Subscriber<sensor_msgs::Imu> sub1;
+    Subscriber<nav_msgs::Odometry> sub2;
+    typedef sync_policies::ApproximateTime<sensor_msgs::Imu, nav_msgs::Odometry> MySyncPolicy;
+    typedef Synchronizer<MySyncPolicy> Sync;
+    boost::shared_ptr<Sync> sync;
+
+    ros::NodeHandle nh;
+    struct curr_state
+    {
+        float x;
+        float y;
+        float angle;
+    };
     
 
     public:
-    Kalmanfilter() : nh() {
-        sub_imu = nh.subscribe("/imu/data", 100, &Kalmanfilter::callback1, this);
-        sub_odom = nh.subscribe("/odom", 100, &Kalmanfilter::callback2, this);
+    Kalmanfilter() : nh() , sub1(nh, "/imu/data", 1000), sub2(nh, "/odom", 1000) {
+    
+        //sub_imu = nh.subscribe("/imu/data", 100, &Kalmanfilter::callback1, this);
+        //sub_odom = nh.subscribe("/odom", 100, &Kalmanfilter::callback2, this);
+
+        sync.reset(new Sync(MySyncPolicy(1000), sub1, sub2));
+        //sync->registerCallback(boost::bind(&Kalmanfilter::callback, this, _1, _2));
+
         // information output
         ROS_INFO("Node started...");
-        imu_msg.orientation.x = 0;
         ros::spin();
     };
 
-    /*    Header header
+        void callback(const sensor_msgs::Imu& msg_imu, const nav_msgs::Odometry& msg_odom) {
+        ROS_INFO("test");
 
-    geometry_msgs/Quaternion orientation
-    float64[9] orientation_covariance # Row major about x, y, z axes
+         /* Imu
 
-    geometry_msgs/Vector3 angular_velocity
-    float64[9] angular_velocity_covariance # Row major about x, y, z axes
+        Header header
 
-    geometry_msgs/Vector3 linear_acceleration
-    float64[9] linear_acceleration_covariance # Row major x, y z */
+        geometry_msgs/Quaternion orientation
+        float64[9] orientation_covariance # Row major about x, y, z axes
 
+        geometry_msgs/Vector3 angular_velocity
+        float64[9] angular_velocity_covariance # Row major about x, y, z axes
 
+        geometry_msgs/Vector3 linear_acceleration
+        float64[9] linear_acceleration_covariance # Row major x, y z */
 
-    /*     Header header
-    string child_frame_id
-    geometry_msgs/PoseWithCovariance pose
-    geometry_msgs/TwistWithCovariance twist */
+        /* Odometry
 
-    void callback1(const sensor_msgs::Imu& msg_imu) {
-        ROS_INFO("test1");
-        std::cout << "test" << std::endl;
-        /*static tf2_ros::TransformBroadcaster br;
-        geometry_msgs::TransformStamped transformStamped;
-
-        transformStamped.header.stamp = ros::Time::now();
-        transformStamped.header.frame_id = "world";
-        */
+        Header header
+        string child_frame_id
+        geometry_msgs/PoseWithCovariance pose
+        geometry_msgs/TwistWithCovariance twist */
     }
-
-    void callback2(const nav_msgs::Odometry& msg_odom) {
-        ROS_INFO("test2");
-        /*static tf2_ros::TransformBroadcaster br;
-        geometry_msgs::TransformStamped transformStamped;
-
-        transformStamped.header.stamp = ros::Time::now();
-        transformStamped.header.frame_id = "world";
-        */
-    }
+    
 };
 
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "kalman_node");
-
     Kalmanfilter k;
     ros::spin();
     return 0;
